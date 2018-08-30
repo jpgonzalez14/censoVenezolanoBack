2b175a40dd5b48d90f58dde7566e15f379fdb8e3
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2018-08-29 19:31:19 -0500

    mongodb conection

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
+'use strict'
 var express = require('express');
+var express = require('mongoose');
 var router = express.Router();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/censo', {useMongoClient: true}).then(()=>{
+
+  console.log('La conexion a la base de datos se realizo correctamente');
+
+}).catch(err=>console.log(err));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

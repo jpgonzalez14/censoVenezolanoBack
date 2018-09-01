2185af3a6714ecd77ad28d0b1fe92923fe13833f
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2018-09-01 16:20:55 -0500

    user login
    email verification

--- controllers/user.js
+++ controllers/user.js
@@ -7,58 +7,105 @@ var User = require('../models/user');
 
 
 //actions
-function prueba(req,res){
+function prueba(req, res) {
   res.status(200).send({
     message: 'probando controlador usuarios y accion prueba'
   });
 }
-function saveUser(req,res){
+
+function saveUser(req, res) {
   //create object user
   var user = new User();
   //user params
   var params = req.body;
 
-
-  if(params.password && params.name && params.org && params.country && params.city && params.email){
+  if (params.password && params.name && params.org && params.country && params.city && params.email) {
     //asign params to user object
     user.name = params.name;
     user.org = params.org;
     user.country = params.country;
     user.city = params.city;
-    user.email = params.email;
+    user.email = params.email.toLowerCase();
     user.role = 'ROLE_USER';
-    //encrypt password
-    bcrypt.hash(params.password, null, null, function(err, hash){
-      user.password = hash;
-      //save user in database
-      user.save((err, userStored)=>{
-        if (err) {
-          res.status(500).send({
-            message: 'error al guardar el usuario'
+
+    User.findOne({email: user.email.toLowerCase()}, (err,isUser)=>{
+      if (err) {
+        res.status(500).send({
+          message: 'error al comprobar el usuario'
+        });
+      } else {
+        if (!isUser) {
+          //encrypt password
+          bcrypt.hash(params.password, null, null, function(err, hash) {
+            user.password = hash;
+            //save user in database
+            user.save((err, userStored) => {
+              if (err) {
+                res.status(500).send({
+                  message: 'error al guardar el usuario'
+                });
+              } else {
+                if (!userStored) {
+                  res.status(404).send({
+                    message: 'no se ha registrado el usuario'
+                  });
+                } else {
+                  res.status(200).send({
+                    user: userStored
+                  });
+                }
+              }
+            });
           });
         } else {
-          if (!userStored) {
-            res.status(404).send({
-              message: 'no se ha registrado el usuario'
-            });
-          } else {
-            res.status(200).send({
-              user: userStored
-            });
-          }
+          res.status(200).send({
+            message: 'el usuario ya existe'
+          });
         }
-      });
+      }
     });
-
   } else {
     res.status(200).send({
       message: 'verifique que envio todos los datos correctamente'
     });
   }
-
 }
+
+
+function login(req, res) {
+
+  //user params
+  var params = req.body;
+
+  if (params.email) {
+
+    var email = params.email;
+
+    User.findOne({email: email.toLowerCase()}, (err,user)=>{
+      if (err) {
+        res.status(500).send({
+          message: 'error al comprobar el usuario'
+        });
+      } else {
+        if (user) {
+          res.status(200).send({user});
+        } else {
+          res.status(404).send({
+            message: 'el usuario no existe'
+          });
+        }
+      }
+    });
+  } else {
+    res.status(200).send({
+      message: 'verifique que envio todos los datos correctamente'
+    });
+  }
+}
+
 //export objects
 module.exports = {
   prueba,
-  saveUser
+  saveUser,
+  login
 };
--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,12 @@ var router = express.Router();
 
 router.get('/controlador', UserController.prueba);
 router.post('/register', UserController.saveUser);
+router.post('/login', UserController.login);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/prueba', function(req, res, next) {
-  res.status(200).send({
-    message: 'probando hfkjhdfksjhkjshfshfhksjhfjkshfwhfdbvhiubkjhfreh'
-  });
-});
 
 module.exports = router;

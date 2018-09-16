037cc181f045c64aeaefb3cd7d84a87440198499
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-09-16 12:08:24 -0500

    añadida la ruta de obtener censos

--- routes/censos.js
+++ routes/censos.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/censo', CensoController.saveCenso);
+router.get('/listcensos, CensoController.getcensos')
 
 
 module.exports = router;

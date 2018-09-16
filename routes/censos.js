4c6b13549c65a9dfeb1c69b94650170304cd13f5
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-09-16 12:10:25 -0500

    Update censos.js

--- routes/censos.js
+++ routes/censos.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/censo', CensoController.saveCenso);
-router.get('/listcensos', CensoController.getCensos)
+router.get('/listcensos', CensoController.getCensos);
 
 
 module.exports = router;

c2481eaf1d1702f001105a30e882135acbb6c5ba
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2018-09-18 07:58:33 -0500

    changes

--- censo-venezolano-front/src/store.js
+++ censo-venezolano-front/src/store.js
@@ -1,17 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-//root reducer
 import rootReducer from './reducers';
-
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-
+const store = createStore(rootReducer,initialState,compose(
+ applyMiddleware(...middleware),
+ window._REDUX_DEVTOOLS_EXTENSION_ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+));
 export default store;
